5ca6106e9c59af7868b5dc37c68eb61b98600814
monicatevy <monicatevy.sen@pm.me>
2021-05-24 15:24:17 +0700

    Création des packages MVC

---
+++ MediaTek/controleur/Controle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTek.controleur
+{
+    public class Controle
+    {
+        public Controle()
+        {
+
+        }
+    }
+}
